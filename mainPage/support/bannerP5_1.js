af7159ca3b7b2bb03454f26f0f8464b6b01393e4
vezzzingdesktop <1932966162@qq.com>
2021-08-09 16:40:22 +0800

    game and gui

--- mainPage/support/bannerP5_1.js
+++ mainPage/support/bannerP5_1.js
@@ -1,117 +1,52 @@
-var vehicleList=[];
-function getNormalPoint(p,a,b){
-    var ap=p5.Vector.sub(p,a);
-    var ab=p5.Vector.sub(b,a);
-    ab.normalize();
-    ab.mult(ap.dot(ab));
-    normalPoint=p5.Vector.add(a,ab);
-    return(normalPoint);
-}
-function Vehicle(positionX,positionY,vx,vy){
-    this.position=createVector(positionX,positionY);
-    this.velocity=createVector(vx,vy);
-    this.acceleration=createVector(0,0);
-    this.size=3;
-    this.maxSpeed=8;
-    this.maxForce=0.7;
-
+var headingMoverList=[];
+function HeadingMover(){
+    this.position=createVector(width/2,height/2);
+    this.velocity=createVector(0,0);
+    //this.acceleration=createVector(0,0.1);
     this.update=function(){
+        this.acceleration=p5.Vector.random2D();
+        this.acceleration.mult(0.5);//mult乘以标量延伸向量
         this.velocity.add(this.acceleration);
-        this.velocity.limit(this.maxSpeed);
+        this.velocity.limit(10);
         this.position.add(this.velocity);
-        this.acceleration.mult(0);
-    }
-    this.applyForce=function(force){
-        this.acceleration.add(force);
-    }
-    this.seek=function(targetx,targety,desiredRaduis){
-        var targetPosition=createVector(targetx,targety);
-        desired=p5.Vector.sub(targetPosition,this.position);
-        var d=desired.mag();
-        desired.normalize();
-        if(d<desiredRaduis){
-            var m=map(d,0,desiredRaduis,0,this.maxSpeed);
-            desired.mult(m);
-        }else{
-            desired.mult(this.maxSpeed);
-        }
-        steer=p5.Vector.sub(desired,this.velocity);
-        steer.limit(this.maxForce);
-        this.applyForce(steer);
-    }
-    this.separate=function(vehicleList){
-        var sum=createVector(0,0);
-        var desiredSeparation=100;
-        var count=0;
-        for(var i=0;i<vehicleList.length;i++){
-            var d=p5.Vector.dist(this.position,vehicleList[i].position);
-            if(d>0 && d<desiredSeparation){
-                var diff=p5.Vector.sub(this.position,vehicleList[i].position);
-                diff.normalize();
-                diff.div(d);
-                sum.add(diff);
-                count+=1;
-            }
-        }
-        if(count>0){
-            sum.div(count);
-            sum.normalize();
-            sum.mult(this.maxSpeed);
-            var steer=p5.Vector.sub(sum,this.velocity);
-            steer.limit(this.maxForce);
-            this.applyForce(steer);
-        }
     }
     this.checkEdges=function(){
         if(this.position.x<0 || this.position.x>width){
-            if(this.position.x<0){
-                this.position.x=0;
-            }else if(this.position.x>width){
-                this.position.x=width;
-            }
             this.velocity.x*=-1;
         }
         if(this.position.y<0 || this.position.y>height){
-            if(this.position.y<0){
-                this.position.y=0;
-            }else if(this.position.y>height){
-                this.position.y=height;
-            }
             this.velocity.y*=-1;
         }
     }
     this.display=function(){
+        stroke(sin(this.velocity.mag())*120+120,cos(this.velocity.mag()*2)*120+120,sin(this.velocity.mag()*3)*120+120,100);
+        strokeWeight(2);
         push();
+        rectMode(CENTER);
         translate(this.position.x,this.position.y);
         var angle=atan2(this.velocity.y,this.velocity.x);
-        rotate(angle+PI/2);
-        fill(255,this.velocity.mag()*20,map(this.velocity.mag(),0,this.maxSpeed,232,120),150);
-        noStroke();
-        beginShape();
-        vertex(0,-this.size*2);
-        vertex(-this.size,this.size*2);
-        vertex(this.size,this.size*2);
-        endShape();
+        rotate(angle-PI/2);
+        line(0,-30,0,0);
+        line(-10,-10,0,0);
+        line(10,-10,0,0);
         pop();
     }
 }
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    smooth();
-    for(var i=0;i<300;i++){
-        vehicleList.push(new Vehicle(random(350,width-350),random(300,height-300),random(-3,3),random(-3,3)));
+    noStroke();
+    for(var i=0;i<50;i++){
+        headingMoverList.push(new HeadingMover);
     }
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 function draw(){
-    background(255,50);
-    for(var i=0;i<vehicleList.length;i++){
-        vehicleList[i].checkEdges();
-        vehicleList[i].seek(mouseX,mouseY,200);
-        vehicleList[i].separate(vehicleList);
-        vehicleList[i].update();
-        vehicleList[i].display();
+    background(249,22);
+    for(var i=0;i<headingMoverList.length;i++){
+        headingMoverList[i].update();
+        headingMoverList[i].checkEdges();
+        headingMoverList[i].display();
     }
 }
--- mainPage/support/bannerP5_2.js
+++ mainPage/support/bannerP5_2.js
@@ -1,50 +1,51 @@
-var movers=[];
-var slocation=null;
-var svelocity=null;
-function Mover(){
-    this.position=createVector(random(0,width),random(0,height));
-    this.velocity=createVector(3,3);
-    this.r=(random(0,255));
-    this.g=(random(125,255));
-    this.b=(random(125,255));
-
-    //this.acceleration=createVector(0,0);
-    this.update=function(){
-        this.acceleration=createVector(mouseX-this.position.x,mouseY-this.position.y)
-        this.acceleration.mult(0.001*random(0.5,3));
-        this.velocity.add(this.acceleration);
-        this.velocity.limit(10);
-        this.position.add(this.velocity);
-    }
-    this.checkEdges=function(){
-        if(this.position.x<0 || this.position.x>width){
-            this.velocity.x*=-1;
-        }
-        if(this.position.y<0 || this.position.y>height){
-            this.velocity.y*=-1;
-        }
+var oscillatorList=[];
+var rtx=0;
+var rty=0;
+var rtz=0;
+function Oscillator(velocityX,velocityY,velocityZ,ampitudeX,ampitudeY,ampitudeZ){
+    this.angle=createVector(0,0,0);
+    this.velocity=createVector(velocityX,velocityY,velocityZ);
+    this.ampitude=createVector(ampitudeX,ampitudeY,ampitudeZ);
+    this.oscillate=function(){
+        this.angle.add(this.velocity);
     }
     this.display=function(){
-        //stroke(0);
-        fill(this.velocity.mag()*12,this.g,this.b);
-        circle(this.position.x,this.position.y,6);
+        var x=sin(this.angle.x)*this.ampitude.x;
+        var y=sin(this.angle.y)*this.ampitude.y;
+        var z=sin(this.angle.z)*this.ampitude.z;
+        stroke(1);
+        //line(0,0,0,x,y,z);
+        push();
+        translate(x,y,z);
+        sphere(20);
+        pop();
     }
 }
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    noStroke();
-    for(var i=0;i<30;i++){
-        movers.push(new Mover)
+    createCanvas(windowWidth, windowHeight,WEBGL);
+    smooth();
+    for(var i=0;i<11;i++){
+        oscillatorList.push(new Oscillator(random(-0.1,0.1),random(-0.1,0.1),random(-0.1,0.1),random(-300,300),random(-300,300),random(-300,300)));
     }
 }
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 function draw(){
-    background(251);         
-    for(var i=0;i<movers.length;i++){
-        movers[i].update();
-        movers[i].checkEdges();
-        movers[i].display();
+
+    rotateX(rtx);
+    rotateY(rty);
+    rotateZ(rtz);
+    background(249,1)
+    for(var i=0;i<oscillatorList.length;i++){
+        oscillatorList[i].oscillate();
+        oscillatorList[i].display();
     }
+}
+function mouseMoved(){
+    rty=map(mouseX,-500,500,-2*PI,0);
+    rotateY(rty);
+    rtx=map(mouseY,500,-500,-2*PI,0);
+    rotateY(rtx);
+    return false;
 }
