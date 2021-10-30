86421594fe0fc8ad67fe4a4edf9d155ab0532db4
vezzzingdesktop <1932966162@qq.com>
2021-10-30 22:39:21 +0800

    add projects

---
+++ codeBet/page_4/DED_calculator2dFunction/functions.js
@@ -0,0 +1,20 @@
+var functions=[
+
+
+
+{
+name:"静态-基本三角函数1",
+preload:`
+background(0);
+drawAxis();
+drawMousePoint(drawText=true,drawLine=true,drawDt=true);
+`,
+ifunction:`
+sin(x);
+`,
+cfunction:`
+drawFunction();
+`
+},
+
+]
